57bb998496ac5ead0ebe4ee557d5830c7d754285
HANZALAH <HANZALAH@DESKTOP-6BJDJ1A>
2025-04-29 10:46:32 +0500

    new changes

--- Dashboard.aspx.cs
+++ Dashboard.aspx.cs
@@ -4,10 +4,13 @@ using System.Data.SqlClient;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
 namespace Webpage
 {
@@ -29,34 +32,74 @@ namespace Webpage
         {
             if (FileUpload1.HasFile)
             {
-                string filename = Path.GetFileName(FileUpload1.FileName);
-                string folderPath = "UserImages/";
-                string filePath = folderPath + filename;
+                string filename = Guid.NewGuid().ToString() + Path.GetExtension(FileUpload1.FileName);
+                string blobContainerName = "userimages"; // make sure it's lowercase and already created in Azure
+                string email = Session["userEmail"].ToString();
 
-                // Save file
-                FileUpload1.SaveAs(Server.MapPath("~/" + filePath));
+                // Azure Blob Storage setup
+                string connectionString = ConfigurationManager.AppSettings["AzureBlobStorageConnectionString"];
+                BlobContainerClient container = new BlobContainerClient(connectionString, blobContainerName);
+                container.CreateIfNotExists(PublicAccessType.Blob);
 
-                // Save path to DB
-                string email = Session["userEmail"].ToString(); // assuming you store email on login
+                BlobClient blob = container.GetBlobClient(filename);
+
+                using (Stream stream = FileUpload1.PostedFile.InputStream)
+                {
+                    blob.Upload(stream, overwrite: true);
+                }
+
+                string blobUrl = blob.Uri.ToString();
+
+                // Save URL to DB
                 string query = "INSERT INTO UserImages (UserEmail, ImagePath) VALUES (@Email, @Path)";
                 List<SqlParameter> parameters = new List<SqlParameter>()
-                {
-                    new SqlParameter("@Email", email),
-                    new SqlParameter("@Path", filePath)
-                };
+        {
+            new SqlParameter("@Email", email),
+            new SqlParameter("@Path", blobUrl)
+        };
 
                 new DbCon().UDI(query, parameters);
 
-                lblUploadStatus.Text = "Image uploaded successfully!";
+                lblUploadStatus.Text = "Image uploaded to Azure Blob Storage!";
                 lblUploadStatus.ForeColor = Color.Green;
 
-                LoadImages(); // refresh image list
+                LoadImages(); // refresh list
             }
             else
             {
                 lblUploadStatus.Text = "Please select an image.";
                 lblUploadStatus.ForeColor = Color.Red;
             }
+            //if (FileUpload1.HasFile)
+            //{
+            //    string filename = Path.GetFileName(FileUpload1.FileName);
+            //    string folderPath = "UserImages/";
+            //    string filePath = folderPath + filename;
+
+            //    // Save file
+            //    FileUpload1.SaveAs(Server.MapPath("~/" + filePath));
+
+            //    // Save path to DB
+            //    string email = Session["userEmail"].ToString(); // assuming you store email on login
+            //    string query = "INSERT INTO UserImages (UserEmail, ImagePath) VALUES (@Email, @Path)";
+            //    List<SqlParameter> parameters = new List<SqlParameter>()
+            //    {
+            //        new SqlParameter("@Email", email),
+            //        new SqlParameter("@Path", filePath)
+            //    };
+
+            //    new DbCon().UDI(query, parameters);
+
+            //    lblUploadStatus.Text = "Image uploaded successfully!";
+            //    lblUploadStatus.ForeColor = Color.Green;
+
+            //    LoadImages(); // refresh image list
+            //}
+            //else
+            //{
+            //    lblUploadStatus.Text = "Please select an image.";
+            //    lblUploadStatus.ForeColor = Color.Red;
+            //}
         }
 
         private void LoadImages()
